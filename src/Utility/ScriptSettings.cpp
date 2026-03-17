9bc952ddbccc28cbd709dfcb8efc0704b14c0d30
ryanm2711 <ryanmaybury1234@gmail.com>
2026-03-17 19:36:37 +0000

    Added initial code for script settings

--- src/Utility/ScriptSettings.cpp
+++ src/Utility/ScriptSettings.cpp
@@ -1,29 +1,105 @@
 #include "ScriptSettings.h"
+#include <iostream>
+#include <stdexcept>
 
 void Utility::ScriptSettings::Load()
 {
-	m_IniFile.LoadFile(m_IniPath.c_str());
+	SI_Error response = m_IniFile.LoadFile(m_IniPath.c_str());
+	if (response < 0) {
+		// Handle proper reporting
+		switch (response) {
+		case -1:
+			std::cout << "Failed to load file!\n";
+			break;
+		case -2:
+			std::cout << "Failed to load file: Out of memory!\n";
+			break;
+		case -3:
+			std::cout << "Failed to load file!\n";
+			break;
+		}
 
-	for (auto& setting : m_RegisteredSettings) {
-		const wchar_t* result = m_IniFile.GetValue(setting.section.c_str(), setting.key.c_str());
+		// If file hasn't loaded properly, let's make a copy.
+		Save();
+	}
+	else {
+		// Ini loaded successfully, now let's compare data between our registered settings and ini
 
-		if (result != nullptr) {
+		for (Setting& setting : m_RegisteredSettings) {
+			const wchar_t* result = m_IniFile.GetValue(setting.section.c_str(), setting.key.c_str());
+			std::wstring s_result{ result };
+
+			if (result == nullptr) {
+				std::cerr << "result is invalid for ini, wrong input?\n";
+				continue;
+			}
+
+			// Send ini results to variable
+			try {
+				if (setting.type == L"float") {
+					float parsedValue = std::stof(s_result);
+					// Cast void* to a float* so it's writable
+					float* target = static_cast<float*>(setting.targetVariable);
+					// Assign value
+					*target = parsedValue;
+				} // Repeat the same for the other data types
+				else if (setting.type == L"int" || setting.type == L"short" || setting.type == L"long") {
+					long parsedValue = std::stoi(s_result);
+					long* target = static_cast<long*>(setting.targetVariable);
+					*target = parsedValue;
+				}
+				else if (setting.type == L"unsigned long" || setting.type == L"DWORD") {
+					unsigned long parsedValue = std::stoul(s_result);
+					DWORD* target = static_cast<DWORD*>(setting.targetVariable);
+					*target = parsedValue;
+				}
+				else {
+					throw std::invalid_argument("Unsupported type");
+				}
+			}
+			catch (const std::invalid_argument& e) {
+				std::wcerr << "INI Error: Invalid value for " << setting.key << " " << e.what() << "\n";
+				// We don't crash; we just keep the default value already in *target
+			}
+			catch (const std::out_of_range& e) {
+				std::wcerr << "INI Error: Value for " << setting.key << " is too large/small." << "\n";
+			}
+			catch (...) {
+				std::cerr << "An unknown error occurred while loading settings." << std::endl;
+			}
 		}
 	}
 }
 
-template<typename T>
-void Utility::ScriptSettings::RegisterSetting(const char* section, const char* key, T defaultValue, T* target)
+void Utility::ScriptSettings::Save()
 {
-	*target = defaultValue;
+	CSimpleIni newIniFile;
+	newIniFile.SetUnicode(true);
 
-	// Store info so can be used later
-	Setting newSetting;
-	newSetting.section = section;
-	newSetting.key = key;
-	newSetting.defaultValue = defaultValue;
-	newSetting.targetVariable = &target;
-	newSetting.type = typeid(T).name();
+	for (Setting& setting : m_RegisteredSettings) {
+		std::wstring s_value;
+		if (setting.type == L"float") {
+			auto value = *static_cast<float*>(setting.targetVariable);
+			s_value = std::to_wstring(value);
+		}
+		else if (setting.type == L"int" || setting.type == L"short" || setting.type == L"long") {
+			auto value = *static_cast<long*>(setting.targetVariable);
+			s_value = std::to_wstring(value);
+		}
+		else if (setting.type == L"unsigned long" || setting.type == L"DWORD") {
+			auto value = *static_cast<DWORD*>(setting.targetVariable);
+			s_value = std::to_wstring(value);
+		}
 
-	m_RegisteredSettings.push_back(newSetting);
+		SI_Error error = newIniFile.SetValue(setting.section.c_str(), setting.key.c_str(), s_value.c_str(), setting.comment.c_str(), false);
+		if (error < 0) {
+			// TODO: Proper error handling here
+			std::wcerr << "Failed to save setting {SECTION: " << setting.section << ", KEY: " << setting.key << "}";
+		}
+	}
+
+	std::string data;
+	newIniFile.Save(data, false);
+
+	newIniFile.SaveFile(m_IniPath.c_str());
 }
--- src/Utility/ScriptSettings.h
+++ src/Utility/ScriptSettings.h
@@ -1,5 +1,7 @@
-#pragma once
+	#pragma once
 #include "../vendor/SimpleIni.h"
+
+#include <typeinfo>
 #include <string>
 #include <vector>
 
@@ -7,6 +9,7 @@ namespace Utility {
 	struct Setting {
 		std::wstring section;
 		std::wstring key;
+		std::wstring comment;
 		std::wstring defaultValue;
 		void* targetVariable;
 		std::wstring type;
@@ -16,9 +19,43 @@ namespace Utility {
 	public:
 		ScriptSettings(std::string iniPath) : m_IniPath(iniPath) {};
 
-		template <typename T>
-		void RegisterSetting(const char* section, const char* key, T defaultValue, T* target);
 		void Load();
+		void Save();
+
+		template <typename T>
+		inline void RegisterSetting(const wchar_t* section, const wchar_t* key, T defaultValue, T* target, const wchar_t* comment=L"") {
+			*target = defaultValue;
+
+			// Store info so can be used later
+			Setting newSetting;
+
+			// Convert data types to wstring
+			std::wstring s_section{ section };
+			std::wstring s_key{ key };
+			std::wstring s_comment{ comment };
+
+			newSetting.section = s_section;
+			newSetting.key = s_key;
+			newSetting.comment = s_comment;
+
+
+			// Convert the T value to wstring
+			if constexpr (std::is_same_v<T, const wchar_t*> || std::is_same_v<T, std::string>) {
+				// Handle strings
+				newSetting.defaultValue = std::wstring{ defaultValue };
+			} 
+			else {
+				newSetting.defaultValue = std::to_wstring(defaultValue);
+			}
+
+			// Store pointer itself
+			newSetting.targetVariable = static_cast<void*>(target);
+
+			std::string t_name = typeid(T).name();
+			newSetting.type = std::wstring(t_name.begin(), t_name.end());
+
+			m_RegisteredSettings.push_back(newSetting);
+		}
 	private:
 		CSimpleIni m_IniFile;
 		std::vector<Setting> m_RegisteredSettings;
