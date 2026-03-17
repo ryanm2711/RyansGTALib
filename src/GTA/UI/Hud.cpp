cb65c3ddbf859002ea51b1842705eb947de85453
ryanm2711 <ryanmaybury1234@gmail.com>
2026-03-17 11:43:54 +0000

    Added function to draw text anywhere on screen

--- src/GTA/UI/Hud.cpp
+++ src/GTA/UI/Hud.cpp
@@ -18,3 +18,13 @@ void GTA::UI::ShowMessage(std::string text, bool emitSound, int duration) {
 	HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME((char*)text.c_str());
 	HUD::END_TEXT_COMMAND_DISPLAY_HELP(0, false, emitSound, duration);
 }
+
+void GTA::UI::ShowText(std::string text, float x, float y, GTA::UI::Color color, int font, float scale)
+{
+	HUD::SET_TEXT_FONT(font);
+	HUD::SET_TEXT_SCALE(1.0f, scale); // p0 is unknown what it does
+	HUD::SET_TEXT_COLOUR(color.r, color.g, color.b, color.a);
+	HUD::BEGIN_TEXT_COMMAND_DISPLAY_TEXT("STRING");
+	HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(text.c_str());
+	HUD::END_TEXT_COMMAND_DISPLAY_TEXT(x, y, 0); // Unsure what p2 does
+}
--- src/GTA/UI/Hud.h
+++ src/GTA/UI/Hud.h
@@ -2,7 +2,15 @@
 #include <string>
 
 namespace GTA::UI {
+	struct Color {
+		int r;
+		int g;
+		int b;
+		int a;
+	};
+
 	int ShowNotification(std::string text);
 	void ShowSubtitle(std::string text, int duration = 5);
 	void ShowMessage(std::string text, bool emitSound = true, int duration=-1);
+	void ShowText(std::string text, float x, float y, GTA::UI::Color color={255, 255, 255, 255}, int font=0, float scale=0.5f);
 }
